a7672c039c7146b8dbfdd34dd344f0671ef9dede
지석 박 <qkrwltjr0331@naver.com>
2017-07-17 20:54:21 +0900

    헤드라인 파싱 대충 만들어봄 getHeadlineAsync()로 가져오고, 리턴값은 List<String> 수정필요할것

---
+++ Wimi/NewsHeadline.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Wimi
+{
+    class NewsHeadline
+    {
+        String Politics_Url = "http://www.iheadlinenews.co.kr/rss/clickTop.xml";
+
+
+
+        private static async Task<List<string>> getHeadlineAsync()
+        {
+           
+            List<String> result = new List<string>(); 
+            int n = 0;
+            XmlDocument xmld = new XmlDocument();
+            try
+            {
+                //xml URL로드
+                //xmld.Load(url);
+                var httpClient = new HttpClient();
+                HttpResponseMessage response = await httpClient.GetAsync(new Uri("http://www.iheadlinenews.co.kr/rss/clickTop.xml"));
+                var jsonStream = await response.Content.ReadAsStreamAsync();
+
+            }
+            catch (NullReferenceException e)
+            {
+                result.Add("참조 페이지가 잘못되었습니다..\n");
+                return result;
+
+            }
+            catch (WebException e)
+            {
+                result.Add("인터넷이 연결되지 않아, 뉴스를 표시할 수 없습니다..\n");
+                return result;
+            }
+            //title값들, 헤드라인의 제목들만 받음.
+            XmlNodeList titleList = xmld.GetElementsByTagName("title");
+            //result += "--뉴스 헤드라인--\n";
+            foreach (XmlNode s in titleList)
+            {
+                result.Add(s.InnerText);
+            }
+
+            return result;
+        }
+    }
+}
