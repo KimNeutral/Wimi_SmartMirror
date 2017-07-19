6b81b5a04178a09f516278ba092a4da1465c000e
KimDaeyong <01rlaeodyd@naver.com>
2017-07-19 09:26:08 +0900

    RandomMusic Updated

--- Wimi/MusicView.cs
+++ Wimi/MusicView.cs
@@ -14,6 +14,11 @@ namespace Wimi
     public partial class MainPage : Page
     {
         Music music = new Music();
+        String[] MusicNames = { "김필", "내 눈에만 보여", "너무너무너무", "뉴페이스",
+                                "라디", "반달", "버즈", "불타오르네", "삐딱하게", "사랑에 빠졌을 때",
+                                "소란했던 시절에", "소유", "쓰담쓰담", "아웃사이드", "오이오",
+                                "응급실", "쩔어", "톰보이", "호구", "아는사람 얘기" };
+        bool[] random = new bool[20];
 
         public async void PlayMusicByEmotionAsync(Emotion emotion)
         {
@@ -24,6 +29,44 @@ namespace Wimi
                 mediaElement.SetSource(s, type);
                 mediaElement.Play();
             }
+        }
+
+        public async Task PlayMusic()
+        {
+            string musicName = RandomMusicName();
+            using (IRandomAccessStream s = await music.GetMusicStream(musicName))
+            {
+                string type = await music.GetMusicMIME(musicName);
+                mediaElement.SetSource(s, type);
+                mediaElement.Play();
+            }
+        }
+
+        public string RandomMusicName()
+        {
+            Random ran = new Random();
+            bool flag = false;
+            foreach(bool v in random)
+            {
+                if (!v)
+                {
+                    flag = true;
+                }
+            }
+            if (!flag)
+            {
+                random = new bool[20];
+            }
+
+            int num = 1;
+            do
+            {
+                num = ran.Next();
+                num = (num % 20) + 1;
+            } while (random[num] != true);
+
+            return MusicNames[num] + ".mp3";
+
         }
 
         public void PauseMusic()
