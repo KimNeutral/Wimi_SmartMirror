c6d1a94761cf2a328b372384370f49e81c54ed03
지석 박 <qkrwltjr0331@naver.com>
2017-12-19 17:21:04 +0900

    DSLocation 라이브러리 추가

---
+++ DSLocation/JsonExample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSLocation
+{
+    public class Users
+    {
+        public int classNum { get; set; }
+        public string name { get; set; }
+        public string profilePic { get; set; }
+        public Location location {get;set;}
+    }
+    public class Location
+    {
+        public int pId { get; set; }
+        public int bId { get; set; }
+        public int floor { get; set; }
+        public string pname { get; set; }
+        public string bname { get; set; }
+        public int rssi { get; set; }
+        public string time { get; set; }
+    }
+
+}
---
+++ DSLocation/LocationHelper.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace DSLocation
+{
+    public class LocationHelper
+    {
+        string uri;
+        
+        public LocationHelper()
+        {
+            uri = @"http://dgswn.us-east-2.elasticbeanstalk.com/";
+        }
+        public async Task<Users> getLocationInfosAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(uri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync("users/location");
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    var locationInfos = JsonConvert.DeserializeObject<Users>(responseContent);
+                    return locationInfos;
+                }catch(Exception e)
+                {
+                    
+                }
+                return null;
+            }
+        }
+    }
+}
