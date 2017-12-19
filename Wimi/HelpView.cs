97a1b60aba4436d2307415c52dc16845ffa7d5ab
KimDaeyong <01rlaeodyd@naver.com>
2017-12-19 20:15:44 +0900

    Icon Bus, Hue Added

--- Wimi/HelpView.cs
+++ Wimi/HelpView.cs
@@ -41,7 +41,7 @@ namespace Wimi
                 new VoiceCommand { ThumbImg = @"Assets\Icons\Siri.png", Feature = "안녕!", Example = "거울앞의 사용자를 인식합니다" },
                 new VoiceCommand { ThumbImg = @"Assets\Icons\Weather.png", Feature = "날씨", Example = "구지면의 날씨를 보여줍니다." },
                 new VoiceCommand { ThumbImg = @"Assets\Icons\News.png", Feature = "뉴스", Example = "가장 최근의 뉴스 몇가지를 보여드립니다." },
-                new VoiceCommand { ThumbImg = @"Assets\Icons\Weather.png", Feature = "버스", Example = "근처 버스정류장의 상태를 알려드립니다." },
+                new VoiceCommand { ThumbImg = @"Assets\Icons\Bus.png", Feature = "버스", Example = "근처 버스정류장의 상태를 알려드립니다." },
                 new VoiceCommand { ThumbImg = @"Assets\Icons\Music.png", Feature = "노래", Example = "노래를 키거나, 끄거나, 잠시 멈춥니다. 또, 볼륨조절이 가능합니다." },
                 new VoiceCommand { ThumbImg = @"Assets\Icons\Hue.png", Feature = "조명(또는 불)", Example = "조명을 키거나 끌 수 있습니다. 다양한 색을 제공합니다." },
             };
