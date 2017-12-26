c1d52b06ffdc8647268ef3a0d43184c4d0a3fe96
KimDaeyong <01rlaeodyd@naver.com>
2017-12-26 13:51:11 +0900

    오늘의 급식안나오는거 수정

--- Wimi/MealView.cs
+++ Wimi/MealView.cs
@@ -16,9 +16,10 @@ namespace Wimi
             Meal meal = new Meal(Regions.Daegu, SchoolType.High, "D100000282");
             gridMeal.Visibility = Windows.UI.Xaml.Visibility.Visible;
             MealList = meal.GetMealMenu();
-            lvMealBreakfast.ItemsSource = MealList[DateTime.Now.Day-1].Breakfast;
-            lvMealLunch.ItemsSource = MealList[DateTime.Now.Day-1].Lunch;
-            lvMealDinner.ItemsSource = MealList[DateTime.Now.Day-1].Dinner;
+            var menu = MealList.Find(x => x.Date.Equals(new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day)));
+            lvMealBreakfast.ItemsSource = menu.Breakfast;
+            lvMealLunch.ItemsSource = menu.Lunch;
+            lvMealDinner.ItemsSource = menu.Dinner;
         }
     }
 }
